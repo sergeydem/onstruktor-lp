e1a61ee7f116042d8598872f1bc8af979e2f9570
sergeydem <s.demjanenko@artorg.com.ua>
2018-05-31 16:42:12 +0300

    3 блок сделал

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 $( document ).ready(function() {
 
+
+    //TABS стр 3
+    wrapper   = $(".tabs-im");
+    tabs      = wrapper.find(".tab-im");
+    tabToggle = wrapper.find(".tab-toggle-im");
+    function openTab() {
+        var content     = $(this).parent().next(".content"),
+            activeItems = wrapper.find(".active");
+        if(!$(this).hasClass('active')) {
+            $(this).add(content).add(activeItems).toggleClass('active');
+            wrapper.css('min-height', '400px' );
+        }
+    };
+
+    tabToggle.on('click', openTab);
+    tabToggle.first().trigger('click');
+//END TABS стр 3
+
+
 });
