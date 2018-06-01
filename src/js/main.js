52e0afb647cbdd5d74b9784725dd48bada2ad175
sergeydem <s.demjanenko@artorg.com.ua>
2018-06-01 11:47:06 +0300

    8 блок сделал

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 
-    //TABS стр 3
+//TABS стр 3
     wrapper   = $(".tabs-im");
     tabs      = wrapper.find(".tab-im");
     tabToggle = wrapper.find(".tab-toggle-im");
@@ -13,10 +13,54 @@ $( document ).ready(function() {
             wrapper.css('min-height', '400px' );
         }
     };
-
     tabToggle.on('click', openTab);
     tabToggle.first().trigger('click');
 //END TABS стр 3
 
+
+//слайдер работ на стр 8
+    $('.slider-work-im').owlCarousel({
+        loop:true,
+        margin:50,
+        autoplay:true,
+        smartSpeed:2000,
+        autoplayTimeout:7000,
+        dots: false,
+        nav: true,
+        navText : ["",""],
+        rewindNav : true,
+        responsive:{
+            0:{
+                items:1,
+                dots: true,
+                nav: false
+            },
+            600:{
+                items:1,
+                dots: true,
+                nav: false
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+//END слайдер работ на стр 8
+
+//блок результаты seo. переключение между слайдами
+    function seoChangeButton(fisrt, second, slideOne, slideTwo) {
+        $('.btn_arrow'+fisrt).addClass('active-button');
+        $('.result-seo .btn_arrow'+ second).removeClass('active-button');
+        $('.result-content'+slideOne).css('display', 'block');
+        $('.result-content'+slideTwo).css('display', 'none');
+    }
+
+    $(".result-seo .btn_arrow.first-btn").on('click', function () {
+        seoChangeButton('.first-btn', '.second-btn' , '.first-slide', '.second-slide');
+    });
+    $(".result-seo .btn_arrow.second-btn").on('click', function () {
+        seoChangeButton( '.second-btn','.first-btn', '.second-slide', '.first-slide');
+    });
+//END блок результаты seo. переключение между слайдами
 
 });
