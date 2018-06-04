7bd3d58837bc77ba6071f6f7199c2b94b1ae3aec
sergeydem <s.demjanenko@artorg.com.ua>
2018-06-04 10:29:52 +0300

    блок 12 - карта

--- src/js/main.js
+++ src/js/main.js
@@ -156,7 +156,61 @@ $( document ).ready(function() {
     redraw();
 //конец таблицы на стр ИМ
 
+
+
+
+
+
+//Google Maps JS
+    var locations = [
+        ['<b>Name 1</b><br>Address Line 1<br>Bismarck, ND 58501<br>Phone: 701-555-1234<br><a href="#" >Link<a> of some sort.', 47.84208743, 35.14388347, 4],
+        ['<b>Name 2</b><br>Address Line 1<br>Fargo, ND 58103<br>Phone: 701-555-4321<br><a href="#" target="_blank">Link<a> of some sort.', 50.42897675, 30.507653, 5]
+        /*
+         * Next point on map
+         *   -Notice how the last number within the brackets incrementally increases from the prior marker
+         *   -Use http://itouchmap.com/latlong.html to get Latitude and Longitude of a specific address
+         *   -Follow the model below:
+         *      ['<b>Name 3</b><br>Address Line 1<br>City, ST Zipcode<br>Phone: ###-###-####<br><a href="#" target="_blank">Link<a> of some sort.', ##.####, -##.####, #]
+         */
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        /* Zoom level of your map */
+        center: new google.maps.LatLng(48.86415588, 31.5623405),
+        /* coordinates for the center of your map */
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+
+//END Google Maps JS
+
+
+
+
+
+
 });
+
+
+
 
 //схема работы. круги. стр 10
 function CustomSlideCircle() {
